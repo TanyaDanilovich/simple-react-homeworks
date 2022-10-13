4cbd6b613718bf43ebb425d56595e80fd5425a21
TanyaDanilovich <tan.danilovich@gmail.com>
2022-10-13 17:13:28 +0300

    Updates

---
+++ precache-manifest.cbac474b343dbec939698f9f932fb664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943c6a817b327bd73fcf23bcdc420fa5",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "ab0f5a46e2686004be8b",
+    "url": "/simple-react-homeworks/static/css/main.e8e28335.chunk.css"
+  },
+  {
+    "revision": "e3943099cfc42689d3d7",
+    "url": "/simple-react-homeworks/static/js/2.f8a5e7b3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-react-homeworks/static/js/2.f8a5e7b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab0f5a46e2686004be8b",
+    "url": "/simple-react-homeworks/static/js/main.370baff2.chunk.js"
+  },
+  {
+    "revision": "35a9557543d5cb14c0c5",
+    "url": "/simple-react-homeworks/static/js/runtime-main.5a451111.js"
+  }
+]);
