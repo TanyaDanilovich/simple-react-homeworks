107f51078b261b5d59530e751d55061752106b70
TanyaDanilovich <tan.danilovich@gmail.com>
2022-10-13 17:02:48 +0300

    homework1 completed

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import classes from "./Message.module.css"
 
-function Message() {
+type MessagePropsType={
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+
+function Message({avatar,name, message,time}: MessagePropsType) {
+    const image: HTMLImageElement = new Image()
     return (
-        <div>
+        <div className={classes.wrapper}>
+            <div className={classes.content}>
+                <div className={classes.avatarContainer}>
+                    <img className={classes.avatar} src={avatar}></img>
+                </div>
+                <div className={classes.align}>
+                    <div className={classes.cornerContainer}>
+                        <div className={classes.cornerInside}></div>
+                        <div className={classes.cornerOutside}></div>
+                    </div>
 
+                    <div className={classes.container}>
+                        <div className={classes.innerAlign}>
+                            <div className={classes.name}>{name}</div>
+                            <div className={classes.messageText}>{message}</div>
+                        </div>
+                        <div className={classes.timeContainer}>
+                            <span className={classes.time}>{time}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
